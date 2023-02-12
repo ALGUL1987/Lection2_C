79796175cf496218de58948ea2ac5434bdacd971
ALGUL1987 <tankipanki1@gmail.com>
2023-02-12 13:22:43 +0300

    Lection2_C#

---
+++ exercise002/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+Найти максимум из 9 чисел
+*/
+int max = 0;
+int[] array = { 10, 12, 443, 423, 51, 64, 57, 87, 91 };
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+}
+Console.WriteLine(max);
